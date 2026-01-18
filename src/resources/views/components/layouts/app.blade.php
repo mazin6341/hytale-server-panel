199af5b5da0e3dc5c0718b78a267c125f4ce44cb
Maazin Faisal <mazin6341@gmail.com>
2026-01-18 19:46:51 +0500

    feat: Installed AlpineJS, Livewire and WireUI

--- src/resources/views/components/layouts/app.blade.php
+++ src/resources/views/components/layouts/app.blade.php
@@ -6,9 +6,12 @@
 
         <title>{{ $title ?? config('app.name', 'Laravel') }}</title>
 
+        @wireUiScripts
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @livewireStyles
     </head>
-    <body class="font-sans antialiased bg-gray-100 text-gray-900">
+    <body class="font-sans antialiased dark:bg-gradient-to-bl dark:from-slate-950 dark:to-slate-900 dark:text-gray-100 bg-gradient-to-bl from-slate-50 to-slate-100 text-gray-900">
         {{ $slot }}
+        @livewireScripts
     </body>
 </html>
