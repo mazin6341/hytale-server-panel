b78ddb1ff0c54e9fd1117872cbd8d7c840f6beb0
Maazin Faisal <mazin6341@gmail.com>
2026-01-25 15:47:51 +0500

    fix: User list UI fix

--- src/resources/views/admin/users/index.blade.php
+++ src/resources/views/admin/users/index.blade.php
@@ -1,6 +1,9 @@
 <x-layouts.dashboard class="flex flex-col gap-4" title="Users - Hytale Web Panel">
-    <div class="w-full flex gap-4 p-2">
-        <h3 class="text-xl flex-1">Users</h3>
+    <div class="w-full flex gap-4 p-4">
+        <div class="flex-1">
+            <h2 class="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Users</h2>
+            <p class="text-sm text-gray-500 dark:text-gray-400">Manage users and their permissions.</p>
+        </div>
         <x-button
             sm
             outline
@@ -11,7 +14,7 @@
         />
     </div>
 
-    <div class="pt-6">
+    <div class="pt-6 px-4">
         <livewire:admin.users.user-list />
     </div>
 </x-layouts.dashboard>
